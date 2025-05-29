9a778ebd365583ecd4baad6f4e6c8a69af65ed7b
Gustavo A Salazar Lima <tavo.slzr@outlook.com>
2025-05-28 23:03:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ written permission of Adobe.
 const viewerConfig = {
     /* Allowed possible values are "FIT_PAGE", "FIT_WIDTH", "TWO_COLUMN", "TWO_COLUMN_FIT_PAGE" or "". */
     defaultViewMode: "FIT_PAGE",
+    embedMode: "FULL_WINDOW",
+    enableFormFilling: true,
+    showDownloadPDF: true,
+    showPrintPDF: true
 };
 
 /* Helper function to render the file using PDF Embed API. */
